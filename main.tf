f051269ba13f86458a1b3f3b21064e487ef3670c
Anastasiia Kovtun <57349964+akovtun-extenda@users.noreply.github.com>
2022-08-16 11:41:35 +0100

    feat: Add temporary array config (#4)

--- main.tf
+++ main.tf
@@ -10,12 +10,14 @@ resource "google_firestore_index" "fs_index" {
 dynamic fields {
   for_each = distinct(flatten([ for k, v in var.indexes : [
                  for i in each.value : {
-                   field_path = i.field_path
-                   order      = i.order
+                   field_path   = i.field_path
+                   order        = i.order
+                   array_config = i.array_config
     }]]))
     content {
-      field_path = lookup(fields.value,"field_path", null)
-      order      = lookup(fields.value,"order",null)
+      field_path   = lookup(fields.value,"field_path", null)
+      order        = lookup(fields.value,"order",null)
+      array_config = lookup(fields.value,"array_config",null)
     }
- }
+  }
 }
