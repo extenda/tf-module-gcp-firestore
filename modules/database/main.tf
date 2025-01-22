e325b6ba66f6c182e1434e52cab0360b74c77d0c
Shayne Clausson <shayne.clausson@extendaretail.com>
2025-01-22 13:39:37 +0100

    add support for creating firestore database

---
+++ modules/database/main.tf
@@ -0,0 +1,20 @@
+resource "google_firestore_database" "database" {
+  project                           = var.project_id
+  name                              = var.database_name
+  location_id                       = var.database_location_id
+  type                              = var.database_type
+  concurrency_mode                  = var.database_concurrency_mode
+  app_engine_integration_mode       = var.database_app_engine_integration_mode
+  point_in_time_recovery_enablement = var.database_point_in_time_recovery_enablement
+  delete_protection_state           = var.database_delete_protection_state
+  deletion_policy                   = var.database_deletion_policy
+}
+
+resource "google_firestore_backup_schedule" "daily-backup" {
+  project  = var.project_id
+  database = google_firestore_database.database.name
+
+  retention = var.database_backup_retention
+
+  daily_recurrence {}
+}
