e4c5a51b67b5d424be6967d3164550640db1113d
Shayne Clausson <shayne.clausson@extendaretail.com>
2025-01-23 20:18:04 +0100

    move variables file

--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -1,5 +1,3 @@
-##General
-
 variable project_id {
   description = "The ID of the project in which the resource belongs"
   type        = string
@@ -9,8 +7,6 @@ variable region {
   type    = string
   default = "europe-west-1"
 }
-
-##Databases
 
 variable database_location_id {
   description = "The location of the database. Available locations are listed at https://cloud.google.com/firestore/docs/locations."
@@ -57,20 +53,3 @@ variable database_backup_retention {
   default = "259200s"
 }
 
-# ##Indexes
-
-# variable index_collection {
-#   description = "The collection name to create index for"
-#   type        = string
-# }
-
-# variable index_query_scope {
-#   type        = string
-#   description = "The scope at which a query is run. Possible values are: COLLECTION, COLLECTION_GROUP"
-#   default     = "COLLECTION"
-# }
-
-# variable indexes {
-#   description = "The map of collection being indexed and the fields supported by this index"
-#   type        = map(list(map(string)))
-# }
---
+++ modules/index/variables.tf
@@ -0,0 +1,29 @@
+##General
+
+variable project_id {
+  description = "The ID of the project in which the resource belongs"
+  type        = string
+}
+
+variable region {
+  type    = string
+  default = "europe-west-1"
+}
+
+##Indexes
+
+variable index_collection {
+  description = "The collection name to create index for"
+  type        = string
+}
+
+variable index_query_scope {
+  type        = string
+  description = "The scope at which a query is run. Possible values are: COLLECTION, COLLECTION_GROUP"
+  default     = "COLLECTION"
+}
+
+variable indexes {
+  description = "The map of collection being indexed and the fields supported by this index"
+  type        = map(list(map(string)))
+}
